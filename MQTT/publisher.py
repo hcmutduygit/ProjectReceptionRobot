258f99c9370b2255c81b114b9c2fda606c94dfdc
hcmutduygit <duyduongbku@hcmut.edu.vn>
2025-07-05 23:25:39 +0700

    test

--- MQTT/publisher.py
+++ MQTT/publisher.py
@@ -2,10 +2,10 @@
 import paho.mqtt.client as mqtt
 
 # Define Variables
-MQTT_HOST = "192.168.1.25"
+MQTT_HOST = "192.168.0.200"
 MQTT_PORT = 1883
 MQTT_KEEPALIVE_INTERVAL = 5
-MQTT_TOPIC = "hello/world"
+MQTT_TOPIC = "hello/nigga"
 MQTT_MSG = "Hello MQTT"
 
 # Define on_connect event Handler
