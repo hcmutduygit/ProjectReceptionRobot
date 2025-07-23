6139e1d83b46096f89e5973a3bed48763ec3cb03
bthw <bthu.fablab@gmail.com>
2025-07-23 18:16:55 +0700

    (cam) auto reload

--- Reception_Robot_GUI/camera.py
+++ Reception_Robot_GUI/camera.py
@@ -1,22 +1,46 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+import threading, requests
+from PyQt6.QtCore import QUrl, QTimer
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtCore import QUrl, QLoggingCategory
-
-# Tat log từ Qt WebEngine
-QLoggingCategory.setFilterRules("qt.webenginecontext=false")
+from PyQt6.QtWidgets import QWidget, QVBoxLayout
 
 class CameraTab(QWidget):
     def __init__(self, ui):
         super().__init__()
         self.ui = ui
+        self.url_str = "http://192.168.0.130:5000/"
+        self.url = QUrl(self.url_str)
 
-        # Tạo widget web
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://youtu.be/3AtDnEC4zak?si=5fRzFzlAMZV1aSGj"))  # web tu may anh http://192.168.0.128:5000/
+        self.browser.setUrl(self.url)
 
-        # Gan web 
         layout = self.ui.camera_2.layout()
         if layout is None:
             layout = QVBoxLayout(self.ui.camera_2)
             self.ui.camera_2.setLayout(layout)
         layout.addWidget(self.browser)
+
+        # Trạng thái kết nối cũ
+        self.connected = True
+
+        # Khởi động luồng kiểm tra kết nối camera
+        self.check_thread = threading.Thread(target=self.check_connection_loop, daemon=True)
+        self.check_thread.start()
+
+    def check_connection_loop(self):
+        while True:
+            try:
+                res = requests.get(self.url_str, timeout=2)
+                if res.status_code == 200:
+                    if not self.connected:
+                        self.connected = True
+                        QTimer.singleShot(0, self.restore_browser)
+                else:
+                    raise Exception("bad status")
+            except:
+                if self.connected:
+                    self.connected = False
+            import time
+            time.sleep(5)
+
+    def restore_browser(self):
+        self.browser.setUrl(self.url)
