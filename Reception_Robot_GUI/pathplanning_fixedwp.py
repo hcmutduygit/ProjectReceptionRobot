69622f83abc824ebf0155686ce59195d73c37f39
bthw <bthu.fablab@gmail.com>
2025-11-27 18:00:21 +0700

    (path) cmt wp6 p2

--- Reception_Robot_GUI/pathplanning_fixedwp.py
+++ Reception_Robot_GUI/pathplanning_fixedwp.py
@@ -30,8 +30,8 @@ class PathPlanner:
             "wp2": ["wp1", "wp3"],
             "wp3": ["wp2", "wp4"],
             "wp4": ["wp3", "wp5"],
-            "wp5": ["wp4", "wp8", "wp6"],
-            # "wp6": ["wp5", "wp7"],
+            "wp5": ["wp4", "wp8"], #, "wp6"
+            # "wp6": ["wp5", "wp7"], 
             # "wp7": ["wp8", "wp6"],
             # "wp8": ["wp5", "wp7"],
         }
