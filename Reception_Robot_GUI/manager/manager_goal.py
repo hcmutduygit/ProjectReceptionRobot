507b071b9c6813b28c7292255109ea4d58663e02
bthw <bthu.fablab@gmail.com>
2025-12-07 17:55:39 +0700

    (path) lower and capitalize goal name

--- Reception_Robot_GUI/manager/manager_goal.py
+++ Reception_Robot_GUI/manager/manager_goal.py
@@ -26,7 +26,7 @@ class GoalManager(BaseManager):
 
     def handle_goal_update(self, name):
         if name:
-            self.goal = name
+            self.goal = name.lower().capitalize()
             self.location_tab.plan_path(name)
             self.ui.robot_status.setText("Guidance")
             self.ui.robot_status_2.setText("Guidance")
